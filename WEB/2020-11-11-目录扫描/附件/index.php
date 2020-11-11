bf64aa0b4a4eae9ea52f3057c962bb4abe1f9fcf
qiuyuhang66 <59474081+qiuyuhang66@users.noreply.github.com>
2020-11-11 09:19:17 +0800

    新增了一道web
    新增了一道web

---
+++ WEB/2020-11-11-目录扫描/附件/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!-- saved from url=(0031)https://bootswatch.com/sketchy/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <title>敏感文件</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./Bootswatch_ Sketchy_files/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="./Bootswatch_ Sketchy_files/custom.min.css">
+    <script type="text/javascript" async="" src="./Bootswatch_ Sketchy_files/ga.js"></script><script>
+
+     var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-23019901-1']);
+      _gaq.push(['_setDomainName', "bootswatch.com"]);
+        _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+
+     (function() {
+       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+     })();
+
+    </script>
+
+  <body>
+
+
+ <div class="container">
+
+      <!-- Containers
+      ================================================== -->
+     
+
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="page-header">
+              <h1 id="containers">敏感文件</h1>
+            </div>
+            <div class="bs-component">
+              <div class="jumbotron">
+                <h1 class="display-3">Hello, CTFer!</h1>
+                <p class="lead">信息搜集之所以重要，是因为其往往会带给我们一些意想不到的东西。</p>
+                <hr class="my-4">
+                <p>hack fun</p>
+                
+          </div>
+        </div>
+
+
+
+    </div>
+
+
+    <script src="./Bootswatch_ Sketchy_files/jquery.min.js"></script>
+    <script src="./Bootswatch_ Sketchy_files/popper.min.js"></script>
+    <script src="./Bootswatch_ Sketchy_files/bootstrap.min.js"></script>
+    <script src="./Bootswatch_ Sketchy_files/custom.js"></script>
+  
+
+</body></html>
